9bd22d60dd49baf580b9df36cc6ca113ab446944
Debbie Cheng <dqrcheng@gmail.com>
2020-12-28 13:35:50 +1100

    Example arg as object working

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,33 @@ import React, { useState } from 'react';
 import { LatLngTuple } from 'leaflet';
 import './App.css';
 import Map from './component/Map';
+import { MarkersInfo } from './type/types';
 
 function App() {
   const starter: LatLngTuple = [-33.86, 151.21]; // latitude, longitude 
   const [defaultPosition, setDefaultPosition] = useState(starter);
+
+  const glebe: LatLngTuple = [-33.878, 151.186]; // test example
+  const markersInfo: MarkersInfo = {
+    places: [
+      {
+        position: glebe,
+        title: "Glebe",
+        description: "Coffee here!",
+      },
+      {
+        position: starter,
+        title: "Sydney",
+        description: "Yay Sydney!",
+      }
+    ],
+  }
   return (
     <div className="App">
       <Map
         position={defaultPosition}
         marker={true}
-        popupText="Sydney Yay!!"
+        markersInfo={markersInfo}
       />
     </div>
   );
