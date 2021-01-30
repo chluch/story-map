938d311625a85232b717bc5ba2d6c24842a83948
Debbie Cheng <dqrcheng@gmail.com>
2021-01-30 23:05:10 +1100

    Init Login

---
+++ src/component/Login/Login.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './Login.module.css'
+
+const Login: React.FC = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.heading}>Login</h1>
+      <form className={styles.loginForm}>
+        <div>
+          <input className={styles.input} type="text" name="username" />
+          <label htmlFor="username" className={styles.label}>Username</label>
+        </div>
+        <div>
+          <input className={styles.input} type="password" name="password" />
+          <label htmlFor="password" className={styles.label}>Password</label>
+        </div>
+      </form>
+
+    </div>
+  );
+}
+
+export default Login;
